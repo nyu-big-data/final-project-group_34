7fcb9344a1afbecb3d0bb13d1998292572e74d7d
Yunjeon-Lee <yjeonlee0905@gmail.com>
2022-04-27 21:21:55 -0400

    [WIP] popularity model

--- Recommendation.py
+++ Recommendation.py
@@ -18,50 +18,45 @@ def main(spark, netID):
     spark : SparkSession object
     netID : string, netID of student to find files in HDFS
     '''
-    print('Lab 3 Example dataframe loading and SQL query')
 
-    # Load the boats.txt and sailors.json data into DataFrame
-    boats = spark.read.csv(f'hdfs:/user/{netID}/boats.txt')
-    sailors = spark.read.json(f'hdfs:/user/{netID}/sailors.json')
+    ratings = spark.read.csv(f'hdfs:/user/{netID}', schema='userId INT, movieId INT, rating DOUBLE, timestamp INT') # TODO timestamep type
+    ratings.createOrReplaceTempView('ratings')
+    avg_scores = spark.sql('SELECT AVG(ratings.rating) FROM ratings GROUP BY ratings.movieId ORDER BY AVG(ratings.rating) DESC')
+    avg_scores.show()
 
-    print('Printing boats inferred schema')
-    boats.printSchema()
-    print('Printing sailors inferred schema')
-    sailors.printSchema()
-    # Why does sailors already have a specified schema?
+    
+    # print('Printing boats inferred schema')
+    # boats.printSchema()
+    # print('Printing sailors inferred schema')
+    # sailors.printSchema()
+    # # Why does sailors already have a specified schema?
 
-    print('Reading boats.txt and specifying schema')
-    boats = spark.read.csv('boats.txt', schema='bid INT, bname STRING, color STRING')
+    # print('Reading boats.txt and specifying schema')
+    # boats = spark.read.csv('boats.txt', schema='bid INT, bname STRING, color STRING')
 
-    print('Printing boats with specified schema')
-    boats.printSchema()
+    # print('Printing boats with specified schema')
+    # boats.printSchema()
 
-    # Give the dataframe a temporary view so we can run SQL queries
-    boats.createOrReplaceTempView('boats')
-    sailors.createOrReplaceTempView('sailors')
-    # Construct a query
-    print('Example 1: Executing SELECT count(*) FROM boats with SparkSQL')
-    query = spark.sql('SELECT count(*) FROM boats')
+    # # Give the dataframe a temporary view so we can run SQL queries
+    # boats.createOrReplaceTempView('boats')
+    # sailors.createOrReplaceTempView('sailors')
+    # # Construct a query
+    # print('Example 1: Executing SELECT count(*) FROM boats with SparkSQL')
+    # query = spark.sql('SELECT count(*) FROM boats')
 
-    # Print the results to the console
-    query.show()
+    # # Print the results to the console
+    # query.show()
 
-    #####--------------YOUR CODE STARTS HERE--------------#####
+    
 
-    #make sure to load reserves.json, artist_term.csv, and tracks.csv
-    #For the CSVs, make sure to specify a schema!
+# Only enter this block if we're in main
+if __name__ == "__main__":
 
-    #question_1_query = ....
+    # Create the spark session object
+    spark = SparkSession.builder.appName('popularity').getOrCreate()
 
+    # Get user netID from the command line
+    netID = getpass.getuser()
 
-# # Only enter this block if we're in main
-# if __name__ == "__main__":
-
-#     # Create the spark session object
-#     spark = SparkSession.builder.appName('part1').getOrCreate()
-
-#     # Get user netID from the command line
-#     netID = getpass.getuser()
-
-#     # Call our main routine
-#     main(spark, netID)
+    # Call our main routine
+    main(spark, netID)
