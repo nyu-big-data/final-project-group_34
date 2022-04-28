30c872df4666dec54ae10d9d16bafbf8c0cea602
Yunjeon Lee <yl7143@hlog-1.hpc.nyu.edu>
2022-04-28 01:36:25 -0400

    [WIP] testing

--- LatentFactorModel.py
+++ LatentFactorModel.py
@@ -18,8 +18,7 @@ def main(spark, netID):
     netID : string, netID of student to find files in HDFS
     '''
 
-    ratings_train = spark.read.csv(f'hdfs:/user/{netID}/train_small_data.csv',
-                             schema='userId INT, movieId INT, rating DOUBLE, timestamp INT')  # TODO timestamep type
+    ratings_train = spark.read.csv(f'hdfs:/user/{netID}/train_small_data.csv', schema='userId INT, movieId INT, rating DOUBLE, timestamp INT')  # TODO timestamep type
     als = ALS(maxIter=5, regParam=0.01, userCol='userId', itemCol='movieId', ratingCol='rating', coldStartStrategy="drop" )
     model = als.fit(ratings_train)
 
