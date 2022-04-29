6c14d2943fc652f4b6d8a21d072006bf5fc971b4
Yunjeon Lee <yl7143@hlog-2.hpc.nyu.edu>
2022-04-28 23:10:44 -0400

    update data format

--- BaselineModel.py
+++ BaselineModel.py
@@ -17,18 +17,18 @@ def main(spark, netID):
     spark : SparkSession object
     netID : string, netID of student to find files in HDFS
     '''
-    ratings = spark.read.csv(f"hdfs:/user/{netID}/train_small_data.csv", schema='userId INT, movieId INT, rating DOUBLE, timestamp INT') # TODO timestamep type
+    ratings = spark.read.csv(f"hdfs:/user/{netID}/train_small_data.csv", schema='id INT, userId INT, movieId INT, rating DOUBLE, timestamp INT') # TODO timestamep type
     ratings.createOrReplaceTempView('ratings')
     avg_scores = spark.sql('SELECT ratings.movieId, AVG(ratings.rating) FROM ratings GROUP BY ratings.movieId ORDER BY AVG(ratings.rating) DESC')
     avg_scores.show()
     # TODO show movie title?
 
-    ratings_val = spark.read.csv(f'hdfs:/user/{netID}/val_small_data.csv', schema='userId INT, movieId INT, rating DOUBLE, timestamp INT') # TODO timestamep type
+    ratings_val = spark.read.csv(f'hdfs:/user/{netID}/val_small_data.csv', schema='id INT, userId INT, movieId INT, rating DOUBLE, timestamp INT') # TODO timestamep type
     ratings_val.createOrReplaceTempView('ratings_val')
     avg_scores_val = spark.sql('SELECT ratings_val.movieId, AVG(ratings_val.rating) FROM ratings_val GROUP BY ratings_val.movieId ORDER BY AVG(ratings_val.rating) DESC')
     avg_scores_val.show()
 
-    ratings_test = spark.read.csv(f'hdfs:/user/{netID}/test_small_data.csv', schema='userId INT, movieId INT, rating DOUBLE, timestamp INT') # TODO timestamep type
+    ratings_test = spark.read.csv(f'hdfs:/user/{netID}/test_small_data.csv', schema='id INT, userId INT, movieId INT, rating DOUBLE, timestamp INT') # TODO timestamep type
     ratings_test.createOrReplaceTempView('ratings_test')
     avg_scores_test = spark.sql('SELECT ratings_test.movieId, AVG(ratings_test.rating) FROM ratings_test GROUP BY ratings_test.movieId ORDER BY AVG(ratings_test.rating) DESC')
     avg_scores_test.show()
