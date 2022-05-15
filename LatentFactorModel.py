51cc604c89055d21a801b4339e2ca1d3e66d5a12
Yunjeon-Lee <yjeonlee0905@gmail.com>
2022-05-15 16:53:55 -0400

    revert

--- LatentFactorModel.py
+++ LatentFactorModel.py
@@ -18,54 +18,59 @@ def main(spark, netID):
     spark : SparkSession object
     netID : string, netID of student to find files in HDFS
     '''
+    maxIters = [10]
+    regParams = [0.1]
+    for maxIter in maxIters:
+        for regParam in regParams:
+            ratings_train = spark.read.parquet(f'hdfs:/user/{netID}/train_combined_small_set.parquet')
+            ratings_train.createOrReplaceTempView('ratings_train')
+            als = ALS(maxIter=maxIter, regParam=regParam, userCol='userId', itemCol='movieId', ratingCol='rating', coldStartStrategy="drop")
+            model = als.fit(ratings_train)
+            ratings_val = spark.read.parquet(f'hdfs:/user/{netID}/val_small_set.parquet') # TODO timestamep type
+            #ratings_val.createOrReplaceTempView('ratings_val')
+            userSubsetRecs = ratings_val.select("userId").distinct().sort("userId")
+            #print("userSubsetRecs")
+            #userSubsetRecs.show()
+            #predicted = model.transform(ratings_val)
+            predicted = model.recommendForUserSubset(userSubsetRecs, 100)
+            def extractMovieIds(rec):
+                return [row.movieId for row in rec]
 
-    ranks = [10, 50, 100, 200]
-    maxIters = [10, 50, 100]
-    regParams = [0.01, 0.1, 1]
+            extractRecMovieIdsUDF = fn.udf(lambda r: extractMovieIds(r), T.ArrayType(T.IntegerType()))
+            predicted = predicted.select(
+                fn.col('userId').alias('pr_userId'),
+                extractRecMovieIdsUDF('recommendations').alias('rec_movie_id_indices')
+            )
+            #predicted = predicted.rdd.map(lambda obj: (obj.movieId))
+            #print(predicted.take(100))
+            #test2 = spark.createDataFrame(predicted, ["userId", "recommendations"])
+            #print("TEST2")
+            #test2.show()
+            print("PREDICTED")
+            print(predicted)
+            label = ratings_val.groupBy("userId").agg(fn.collect_list('movieId').alias('label'))
+            #print("TO LIST")
+            #label.show()
 
-    for rank in ranks:
-        for maxIter in maxIters:
-            for regParam in regParams:
-                ratings_train = spark.read.parquet(f'hdfs:/user/{netID}/train_combined_small_set.parquet')
-                ratings_train.createOrReplaceTempView('ratings_train')
+            combined = predicted.join(fn.broadcast(label), fn.col('pr_userId') == fn.col('userId'))\
+                .rdd.map(lambda r: (r.rec_movie_id_indices, r.label))
 
-                als = ALS(rank = rank, maxIter=maxIter, regParam=regParam, \
-                          userCol='userId', itemCol='movieId', ratingCol='rating', coldStartStrategy="drop")
-                model = als.fit(ratings_train)
+            # combined = predicted.join(label, ['userId'])
+            # print("COMBINED")
+            # combined.show()
+            #sc = SparkContext("local", "First App")
+            #rdd = sc.parallelize(combined)
+            metrics = RankingMetrics(combined)
+            print('PrecisionAtK: ', metrics.precisionAt(100))
+            print('MAP: ', metrics.meanAveragePrecision)
 
-                ratings_val = spark.read.parquet(f'hdfs:/user/{netID}/val_small_set.parquet') # TODO timestamep type
-                #ratings_val.createOrReplaceTempView('ratings_val')
-                userSubsetRecs = ratings_val.select("userId").distinct().sort("userId")
-                #userSubsetRecs.show()
-                #predicted = model.transform(ratings_val)
-                predicted = model.recommendForUserSubset(userSubsetRecs, 100)
 
-                def extractMovieIds(rec):
-                    return [row.movieId for row in rec]
 
-                extractRecMovieIdsUDF = fn.udf(lambda r: extractMovieIds(r), T.ArrayType(T.IntegerType()))
-                predicted = predicted.select(fn.col('userId').alias('pr_userId'),
-                                             extractRecMovieIdsUDF('recommendations').alias('rec_movie_id_indices'))
-                print("PREDICTED")
-                print(predicted)
-
-                label = ratings_val.groupBy("userId").agg(fn.collect_list('movieId').alias('label'))
-                #print("TO LIST")
-                #label.show()
-
-                # combined = predicted.join(fn.broadcast(label), fn.col('pr_userId') == fn.col('userId'))\
-                #     .rdd.map(lambda r: (r.rec_movie_id_indices, r.label))
-
-                # metrics = RankingMetrics(combined)
-                # print('rank: ', rank, 'maxIter: ', maxIter, 'regParam: ', regParam, 'alpha: ', alpha)
-                # print('PrecisionAtK: ', metrics.precisionAt(100))
-                # print('MAP: ', metrics.meanAveragePrecision)
-
-                predicted.write.mode('overwrite').parquet(f'hdfs:///user/yl7143/val_ALS_small_predicted_{rank}_{maxIter}_{regParam}.parquet')
-                # predicted = predicted.na.drop()
-                #evaluator = RegressionEvaluator(metricName='rmse', labelCol='label', predictionCol="rec_movie_id_indices")
-                #rmse = evaluator.evaluate(predicted)
-
+            predicted.write.mode('overwrite').parquet('hdfs:///user/yl7143/val_ALS_small_predicted.parquet')
+            # predicted = predicted.na.drop()
+            #evaluator = RegressionEvaluator(metricName='rmse', labelCol='label', predictionCol="rec_movie_id_indices")
+            #rmse = evaluator.evaluate(predicted)
+            #print('maxIter: ', maxIter, 'regParam: ', regParam, 'Root-mean-square error = ' + str(rmse))
 
 
 
